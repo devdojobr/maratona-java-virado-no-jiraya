96f921f3d687b1354ca8b77add71d06ba1037d04
William Suane <william.suane@devdojo.academy>
2020-11-19 19:22:42 +0100

    source code video 09

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
-
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas
+     */
     public static void main(String[] args){
         System.out.println("KA-ME-HA-ME-HA");
     }
