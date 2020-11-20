817269e3714a461c6a29302a951c5a3eb823044e
William Suane <william.suane@devdojo.academy>
2020-11-20 18:47:27 +0100

    source code video 11

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,17 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
         System.out.println("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println("char "+caractere);
     }
 }
