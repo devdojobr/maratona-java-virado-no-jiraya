f4ce15ebb905890e6ce32fd3d2c40938db468e39
William Suane <william.suane@devdojo.academy>
2020-11-24 18:20:00 +0100

    source code video 16

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -5,7 +5,24 @@ public class Aula04Operadores {
         // + - / *
         int numero01 = 10;
         int numero02 = 20;
-        double resultado = numero01 / numero02;
+        double resultado = numero01 / (double) numero02;
         System.out.println(resultado);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10.0;
+        boolean isDezDiferenteDez = 10 != 10.0;
+
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
     }
 }
