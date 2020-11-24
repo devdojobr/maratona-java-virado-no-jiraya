71200c741d30964ea748ed5327b210ab184041b7
William Suane <william.suane@devdojo.academy>
2020-11-24 19:00:38 +0100

    source code video 1

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -13,8 +13,8 @@ public class Aula04Operadores {
         System.out.println(resto);
 
         // < > <= >= == !=
-        boolean isDezMaiorQueVinte = 10 > 20;
-        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezMaiorQueVinte = 10 >= 20;
+        boolean isDezMenorQueVinte = 10 <= 20;
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezIgualDez = 10 == 10.0;
         boolean isDezDiferenteDez = 10 != 10.0;
@@ -24,5 +24,15 @@ public class Aula04Operadores {
         System.out.println("isDezIgualVinte "+isDezIgualVinte);
         System.out.println("isDezIgualDez "+isDezIgualDez);
         System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
+
+        // && (AND) || (or) !
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+
     }
 }
