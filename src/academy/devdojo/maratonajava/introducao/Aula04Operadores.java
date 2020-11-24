c7a6cc512f5bdb4bd54d8f831d807a188064edba
William Suane <william.suane@devdojo.academy>
2020-11-24 17:48:50 +0100

    source code video 15

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+    }
+}
