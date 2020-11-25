5124a811d549cde0c6914c3a3da8bdc567306ebf
William Suane <william.suane@devdojo.academy>
2020-11-25 18:32:55 +0100

    source code video 19

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -38,7 +38,25 @@ public class Aula04Operadores {
         double valorTotalContaPoupanca = 10000;
         float valorPlaystation = 5000F;
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
-        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+        System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
 
+        // = += -= *= /= %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
     }
 }
