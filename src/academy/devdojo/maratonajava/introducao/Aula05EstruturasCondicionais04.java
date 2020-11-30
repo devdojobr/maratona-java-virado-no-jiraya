b3ae03e2beffbc379b5b23bfb1e423c3a88d0c18
William Suane <william.suane@devdojo.academy>
2020-11-30 20:01:50 +0100

    video 24

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -1,20 +1,20 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais04 {
-    public static void main(String[] args) {
-        double salarioAnual = 25000;
-        double valorAnualImposto;
-        double primeiraFaixa = ((9.70)/100);
-        double segundaFaixa = ((37.35)/100);
-        double terceiraFaixa = ((49.50)/100);
-        if(salarioAnual <= 34712){
-            valorAnualImposto = salarioAnual * primeiraFaixa;
-        }else if(salarioAnual > 34712 && salarioAnual < 68507){
-            valorAnualImposto = salarioAnual * segundaFaixa;
-        }else {
-            valorAnualImposto = salarioAnual * terceiraFaixa;
-        }
-        System.out.println("Total a pagar "+valorAnualImposto);
 
+    public static void main(String[] args) {
+        double salarioAnual = 70000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorImposto;
+        if (salarioAnual <= 34712) {
+            valorImposto = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual >= 34713 && salarioAnual <= 68507) {
+            valorImposto = salarioAnual * segundaFaixa;
+        } else {
+            valorImposto = salarioAnual * terceiraFaixa;
+        }
+        System.out.println(valorImposto);
     }
 }
