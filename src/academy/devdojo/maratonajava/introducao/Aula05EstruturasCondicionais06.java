4dda84610a25995f7164d1eb6d48399564663aeb
William Suane <william.suane@devdojo.academy>
2020-12-02 17:30:23 +0100

    video 26

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        // Utilizando switch e dados os valores de 1 a 7, imprima se é dia útil ou final de semana
+        // Considerando 1 como domingo
+        byte dia = 6;
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("FDS");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+    }
+}
