2c821fc10578d4fec89e9ec63d802a1c751ac55e
William Suane <william.suane@devdojo.academy>
2020-12-08 19:08:24 +0100

    video 32

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -6,7 +6,6 @@ public class Aula07Arrays01 {
         idades[0] = 21;
         idades[1] = 15;
         idades[2] = 11;
-        idades[3] = 11;
         System.out.println(idades[0]);
         System.out.println(idades[1]);
         System.out.println(idades[2]);
