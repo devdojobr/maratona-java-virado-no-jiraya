d724429df142b1d39f7818b9c31e5fc51250c55a
William Suane <william.suane@devdojo.academy>
2020-12-08 19:31:01 +0100

    video 33

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double 0
+        // char '\u0000' ' '
+        // boolean false
+        // String null
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
