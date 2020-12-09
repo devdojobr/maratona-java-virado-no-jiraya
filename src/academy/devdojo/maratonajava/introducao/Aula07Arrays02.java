ff79287db6a9ea1b57405ab6ca64e768862b09d0
William Suane <william.suane@devdojo.academy>
2020-12-09 19:16:28 +0100

    video 34

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -8,8 +8,13 @@ public class Aula07Arrays02 {
         // String null
 
         String[] nomes = new String[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
     }
 }
