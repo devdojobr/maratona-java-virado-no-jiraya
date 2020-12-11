b06e8ab0e24a83d924f65c8aea2a4c1302ac66e6
William Suane <william.suane@devdojo.academy>
2020-12-11 17:59:08 +0100

    video 37

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -14,8 +14,16 @@ public class Aula08ArraysMultidimensionais01 {
         dias[1][2] = 31;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length ; j++) {
+                for (int j = 0; j < dias[i].length ; j++) {
                 System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println("------------------------");
+
+        for(int[] arrBase: dias){
+            for (int num: arrBase){
+                System.out.println(num);
             }
         }
     }
