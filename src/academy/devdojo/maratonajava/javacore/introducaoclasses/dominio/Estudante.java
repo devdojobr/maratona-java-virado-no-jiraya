84d2aa5e4b33bd182eaa1521d5a26ae55fbdbd1a
William Suane <william.suane@devdojo.academy>
2020-12-14 19:17:14 +0100

    video 39

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+    }
+}
