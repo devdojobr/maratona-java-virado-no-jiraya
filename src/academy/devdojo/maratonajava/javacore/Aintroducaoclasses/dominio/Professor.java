0741ef5bdec2e6e77bea7660a5796493aab94b2c
William Suane <william.suane@devdojo.academy>
2020-12-15 17:49:36 +0100

    video 41

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Professor.java
@@ -1,8 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
 
-public class Estudante {
-    public String nome = "Zoro";
+public class Professor {
+    public String nome;
     public int idade;
     public char sexo;
-
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Mestre Kami";
+        professor.idade = 140;
+        professor.sexo = 'M';
+
+        System.out.println("Nome " + professor.nome + " idade: " + professor.idade + " sexo:" + professor.sexo);
+    }
+}
