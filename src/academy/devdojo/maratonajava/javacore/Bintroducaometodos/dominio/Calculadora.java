28918dd27c0666cea6f2f3f294f0109d696e0e88
William Suane <william.suane@devdojo.academy>
2020-12-17 18:42:02 +0100

    video 44

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(21 - 2);
+    }
+}
