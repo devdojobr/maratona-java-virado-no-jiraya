5033b1b451b73ff2a687818b24a09fc381403c3b
William Suane <william.suane@devdojo.academy>
2020-12-21 18:33:52 +0100

    video 48

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -34,6 +34,13 @@ public class Calculadora {
             return;
         }
         System.out.println(num1 / num2);
+    }
 
+    public void alteraDoisNumeros(int numero1, int numero2){
+        numero1 = 99;
+        numero2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("Num1 "+numero1);
+        System.out.println("Num2 "+numero2);
     }
 }
