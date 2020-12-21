c8eb19fd6b50364864d55fe19257a6921d1128e4
William Suane <william.suane@devdojo.academy>
2020-12-21 19:48:52 +0100

    video 49

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante){
+        System.out.println("-----------------------");
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.idade);
+    }
+}
