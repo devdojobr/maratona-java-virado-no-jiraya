aab3249cab74aa9bb93ccad3b6b67db7b1772e60
William Suane <william.suane@devdojo.academy>
2020-12-22 19:59:05 +0100

    video 52

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -36,11 +36,27 @@ public class Calculadora {
         System.out.println(num1 / num2);
     }
 
-    public void alteraDoisNumeros(int numero1, int numero2){
+    public void alteraDoisNumeros(int numero1, int numero2) {
         numero1 = 99;
         numero2 = 33;
         System.out.println("Dentro do alteraDoisNumeros");
-        System.out.println("Num1 "+numero1);
-        System.out.println("Num2 "+numero2);
+        System.out.println("Num1 " + numero1);
+        System.out.println("Num2 " + numero2);
+    }
+
+    public void somaArray(int[] numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs(int... numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
     }
 }
