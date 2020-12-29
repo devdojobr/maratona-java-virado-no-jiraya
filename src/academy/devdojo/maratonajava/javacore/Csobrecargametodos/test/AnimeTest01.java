64eaeb40a4b422ffee7c27b3dcd2e7ed99d54a83
William Suane <william.suane@devdojo.academy>
2020-12-29 18:49:35 +0100

    video 57

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Akudama Drive","TV", 12,"Ação");
+        anime.imprime();
+    }
+}
