709eeb502e76e6e7bbaa13034a63714048cda27d
William Suane <william.suane@devdojo.academy>
2020-12-30 17:31:51 +0100

    video 58

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
+        Anime anime2 = new Anime();
+        anime.imprime();
+        anime2.imprime();
+    }
+}
