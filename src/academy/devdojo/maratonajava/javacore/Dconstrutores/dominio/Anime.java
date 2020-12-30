67a8cc8b42da70de438cfdf0e8d5221d8a8adadb
William Suane <william.suane@devdojo.academy>
2020-12-30 18:05:39 +0100

    video 59

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,16 +5,23 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
-    public Anime(String nome, String tipo, int episodios, String genero){
-        System.out.println("Dentro do construtor");
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
-    public Anime(){
 
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public Anime() {
+        System.out.println("Dentro do construtor sem argumentos");
     }
 
     public void imprime() {
@@ -22,6 +29,7 @@ public class Anime {
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public String getGenero() {
--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,9 +5,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "Production IG");
         anime.imprime();
-        anime2.imprime();
     }
 }
