b3560749134f0e75cd92cd3d38011e9d48f327e4
William Suane <william.suane@devdojo.academy>
2021-01-04 17:38:31 +0100

    video 61

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprime() {
+        System.out.println("---------------");
+        System.out.println("Nome " + this.nome);
+        System.out.println("Velocidade máxima " + this.velocidadeMaxima);
+        System.out.println("Velocidade limite " + Carro.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+        Carro.velocidadeLimite = 180;
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}
