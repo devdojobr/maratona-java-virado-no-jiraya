ba193811eb7a0121a1ccd8c357c3d848d1eb38cd
William Suane <william.suane@devdojo.academy>
2021-01-07 18:44:09 +0100

    video 66

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Professor {
+    private String nome;
+
+    public Professor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
