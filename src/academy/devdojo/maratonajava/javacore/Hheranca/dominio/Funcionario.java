1ee5fe177d9606e1b324fe989a25a911495209c5
William Suane <william.suane@devdojo.academy>
2021-01-12 19:26:17 +0100

    video 73

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -4,9 +4,13 @@ public class Funcionario extends Pessoa {
 
     private double salario;
 
-    public void imprime(){
+    public void imprime() {
         super.imprime();
         System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento() {
+        System.out.println("Eu " + this.nome + " recevi o salario de " + this.salario);
     }
 
     public double getSalario() {
--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Pessoa.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Pessoa.java
@@ -1,9 +1,9 @@
 package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
 
 public class Pessoa {
-    private String nome;
-    private String cpf;
-    private Endereco endereco;
+    protected String nome;
+    protected String cpf;
+    protected Endereco endereco;
 
     public void imprime() {
         System.out.println(this.nome);
