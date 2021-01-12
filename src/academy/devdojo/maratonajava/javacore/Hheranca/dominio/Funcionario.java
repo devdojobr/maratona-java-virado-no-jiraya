d8298ee58879156d9ca2909898cbf3cbe643abd0
William Suane <william.suane@devdojo.academy>
2021-01-12 18:59:31 +0100

    video 72

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -4,6 +4,11 @@ public class Funcionario extends Pessoa {
 
     private double salario;
 
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
