7f56a721e5b527dcbfba364ff9376cb3b01f9020
William Suane <william.suane@devdojo.academy>
2021-01-14 18:54:46 +0100

    video 76

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+
+        System.out.println(anime);
+    }
+}
