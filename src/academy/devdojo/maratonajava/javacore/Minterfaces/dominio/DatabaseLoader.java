6d79e24bfaa34296cc9334ba73d9f8493149b0fb
William Suane <william.suane@devdojo.academy>
2021-01-25 18:12:53 +0100

    video 89

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public class DatabaseLoader implements DataLoader, DataRemover {
+    // private -> default -> protected -> public
     @Override
     public void load() {
         System.out.println("Carregando dados do banco de dados");
@@ -14,5 +15,9 @@ public class DatabaseLoader implements DataLoader, DataRemover {
     @Override
     public void checkPermission() {
         System.out.println("Checando permissões no banco de dados");
+    }
+
+    public static void retrieveMaxDataSize(){
+        System.out.println("Dentro do retrieveMaxDataSize na classe DatabaseLoader");
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -1,5 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.test;
 
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DataLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
 
@@ -15,5 +16,8 @@ public class DataLoaderTest01 {
 
         databaseLoader.checkPermission();
         fileLoader.checkPermission();
+
+        DataLoader.retrieveMaxDataSize();
+        DatabaseLoader.retrieveMaxDataSize();
     }
 }
