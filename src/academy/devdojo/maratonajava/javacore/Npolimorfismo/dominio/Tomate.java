5b89bacf0e382a78051ea3f536a68c290d3f5318
William Suane <william.suane@devdojo.academy>
2021-01-26 17:54:58 +0100

    video 91

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
 
 public class Tomate extends Produto {
     public static final double IMPOSTO_POR_CENTO = 0.06;
+    private String dataValidade;
 
     public Tomate(String nome, double valor) {
         super(nome, valor);
@@ -11,5 +12,13 @@ public class Tomate extends Produto {
     public double calcularImposto() {
         System.out.println("Calculando imposto do tomate");
         return this.valor * IMPOSTO_POR_CENTO;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9", 3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("------------");
+
+        Produto produto2 = new Tomate("Americano", 20);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+
+    }
+}
