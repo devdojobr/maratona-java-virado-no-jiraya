6a0766ee5cda595e22256c6423fd3140a5994ad1
William Suane <william.suane@devdojo.academy>
2021-01-27 15:24:10 +0100

    video 93

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
 
@@ -10,5 +11,10 @@ public class CalculadoraImposto {
         System.out.println("Produto: " + produto.getNome());
         System.out.println("Preço: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
+        if (produto instanceof Tomate) {
+            String dataValidade = ((Tomate) produto).getDataValidade();
+
+            System.out.println(dataValidade);
+        }
     }
 }
