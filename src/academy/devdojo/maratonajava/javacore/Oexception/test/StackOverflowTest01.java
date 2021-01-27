da6edf15adcefea3d39689e6771f4081a726d586
William Suane <william.suane@devdojo.academy>
2021-01-27 17:13:03 +0100

    video 95

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
