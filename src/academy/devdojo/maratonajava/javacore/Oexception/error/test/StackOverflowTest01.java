1c1a5aaf045fe1e340c0128c7a5f0aff9c59b12b
William Suane <william.suane@devdojo.academy>
2021-01-27 18:18:39 +0100

    video 96

--- src/academy/devdojo/maratonajava/javacore/Oexception/error/test/StackOverflowTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/error/test/StackOverflowTest01.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.javacore.Oexception.test;
+package academy.devdojo.maratonajava.javacore.Oexception.error.test;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked e Unchecked
+        int[] nums = {1,2};
+        System.out.println(nums[2]);
+    }
+}
