8a8197161a1cf47486909a3eecb0a7e26e1390c0
William Suane <william.suane@devdojo.academy>
2021-02-02 17:58:10 +0100

    video 101

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro do IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+
+
+        try {
+            talvezLanceException();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException{
+
+    }
+}
