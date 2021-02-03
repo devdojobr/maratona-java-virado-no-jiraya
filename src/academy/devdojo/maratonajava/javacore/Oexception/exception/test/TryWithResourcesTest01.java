ea8abe8cc3f3bafd0e031d0c11820c3b2013c06e
William Suane <william.suane@devdojo.academy>
2021-02-03 17:56:07 +0100

    video 103

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/TryWithResourcesTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor1;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor2;
+
+import java.io.*;
+
+public class TryWithResourcesTest01 {
+    public static void main(String[] args) {
+        lerArquivo();
+    }
+
+    public static void lerArquivo() {
+        try (Leitor1 leitor1 = new Leitor1();
+             Leitor2 leitor2 = new Leitor2()) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("teste.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+}
