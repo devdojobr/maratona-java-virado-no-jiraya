a9db0dae2486928e9f9d7ba2a63a06136be83256
William Suane <william.suane@devdojo.academy>
2021-02-08 18:22:40 +0100

    video 109

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -4,7 +4,7 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "William"; //String constant pool
         String nome2 = "William";
-        nome = nome.concat(" Suane");
+        nome = nome.concat(" Suane"); // nome += " Suane"
         System.out.println(nome);
         System.out.println(nome == nome2);
         String nome3 = new String("William"); // 1 variavel de referência, 2 objeto do tipo string, 3 uma string no pool de string
