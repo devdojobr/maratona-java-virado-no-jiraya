247a0772f2f248d104b17f4b1103be1d22969f22
William Suane <william.suane@devdojo.academy>
2021-02-15 17:46:39 +0100

    video 118

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+        String pattern = "'Amsterdam' dd 'de' MMMM 'de' yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        System.out.println(sdf.format(new Date()));
+        try {
+            System.out.println(sdf.parse("Amsterdam 15 de February de 2021"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
