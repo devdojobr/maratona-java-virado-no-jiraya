013cf210110eb9d688415958c2de679e01cd6db7
William Suane <william.suane@devdojo.academy>
2021-02-16 18:55:42 +0100

    video 120

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.CLOCK_HOUR_OF_AMPM));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+    }
+}
