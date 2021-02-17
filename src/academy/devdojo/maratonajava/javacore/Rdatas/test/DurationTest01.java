5cf6fcdfab006d7545049376d0a8ac158307772d
William Suane <william.suane@devdojo.academy>
2021-02-17 15:29:21 +0100

    video 123

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DurationTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class DurationTest01 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2).plusMinutes(8);
+        LocalTime timeNow = LocalTime.now();
+        LocalTime timeMinus7Hours = LocalTime.now().minusHours(7);
+        Duration d1 = Duration.between(now, nowAfterTwoYears);
+        Duration d2 = Duration.between(timeNow, timeMinus7Hours);
+        Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        Duration d4 = Duration.ofDays(20);
+        Duration d5 = Duration.ofMinutes(3);
+        Duration d6 = Duration.of(3, ChronoUnit.HOURS);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
+
+    }
+}
