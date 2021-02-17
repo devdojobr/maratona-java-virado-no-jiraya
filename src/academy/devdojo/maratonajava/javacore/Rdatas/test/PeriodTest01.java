df3be95439afcff45ac6808ac130c31f743c70a5
William Suane <william.suane@devdojo.academy>
2021-02-17 18:19:22 +0100

    video 125

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -4,6 +4,7 @@ import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Local;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
@@ -21,6 +22,7 @@ public class PeriodTest01 {
         System.out.println(p4);
         System.out.println(p5);
         System.out.println(p3.getMonths());
-        System.out.println(Period.between(LocalDate.now(), LocalDate.now().plusDays(p3.getDays())).getMonths());
+        System.out.println(Period.between(now, now.plusDays(p3.getDays())).getMonths());
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.DAYS));
     }
 }
