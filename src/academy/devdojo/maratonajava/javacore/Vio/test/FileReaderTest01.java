1902bb3d31c4d2c750c45824fe075fb51bc3840b
William Suane <william.suane@devdojo.academy>
2021-03-01 18:41:19 +0100

    video 141

--- src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
@@ -8,8 +8,9 @@ public class FileReaderTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try(FileReader fr = new FileReader(file)){
-//            char[] in = new char[30];
-//            fr.read(in);
+//            char[] in = new char[1000];
+//            int size = fr.read(in);
+//            System.out.println(size);
 //            for (char c : in) {
 //                System.out.print(c);
 //            }
