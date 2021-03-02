87d9506489b029cc9e27cae314922323dfccd424
William Suane <william.suane@devdojo.academy>
2021-03-02 18:49:07 +0100

    video 144

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("C:\\Users\\William\\Documents\\devdojo\\maratona-java\\file.txt");
+        Path p2 = Paths.get("C:\\Users\\William\\Documents\\devdojo\\maratona-java","file.txt");
+        Path p3 = Paths.get("C:","Users\\William\\Documents\\devdojo\\maratona-java","file.txt");
+        Path p4 = Paths.get("C:","Users","William","Documents","devdojo","maratona-java","file.txt");
+
+        System.out.println(p1.getFileName().toFile().lastModified());
+        System.out.println(p2.getFileName().toFile().lastModified());
+        System.out.println(p3.getFileName().toFile().lastModified());
+        System.out.println(p4.getFileName().toFile().lastModified());
+    }
+}
