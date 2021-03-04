3396d739468bebda6233b781137b76b4de611d67
William Suane <william.suane@devdojo.academy>
2021-03-04 17:49:41 +0100

    video 148

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/RelativizeTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizeTest01 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("/home/william");
+        Path clazz = Paths.get("/home/william/devdojofoda/OlaMundo.java");
+        Path pathToClazz = dir.relativize(clazz);
+        System.out.println(pathToClazz);
+
+        Path absoluto1 = Paths.get("/home/william");
+        Path absoluto2 = Paths.get("/usr/local");
+        Path absoluto3 = Paths.get("/home/william/devdojofoda/OlaMundo.java");
+        Path relativo1 = Paths.get("temp");
+        Path relativo2 = Paths.get("temp/temp.2021921");
+
+        System.out.println("1 "+absoluto1.relativize(absoluto3));
+        System.out.println("2 "+absoluto3.relativize(absoluto1));
+        System.out.println("3 "+absoluto1.relativize(absoluto2));
+        System.out.println("4 "+relativo1.relativize(relativo2));
+        System.out.println("5 "+absoluto1.relativize(relativo1));
+
+
+    }
+}
--- src/academy/devdojo/maratonajava/javacore/Wnio/test/ResolveTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/ResolveTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Wnio.test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTest03 {
+public class ResolveTest01 {
     public static void main(String[] args) {
         Path dir = Paths.get("home/william");
         Path arquivo = Paths.get("dev/arquivo.txt");
