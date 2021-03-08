3fdd8e8e3b76073b383ecc0724ed180aaf6cd9d3
William Suane <william.suane@devdojo.academy>
2021-03-08 18:11:20 +0100

    video 151

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/DosFileAttributesTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
+
+public class DosFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("pasta/teste.txt");
+        if (Files.notExists(path)) Files.createFile(path);
+        DosFileAttributes dosFileAttributes = Files.readAttributes(path, DosFileAttributes.class);
+        System.out.println(dosFileAttributes.isHidden());
+        System.out.println(dosFileAttributes.isReadOnly());
+
+        DosFileAttributeView fileAttributeView = Files.getFileAttributeView(path, DosFileAttributeView.class);
+        fileAttributeView.setHidden(true);
+        fileAttributeView.setReadOnly(true);
+        System.out.println(fileAttributeView.readAttributes().isHidden());
+        System.out.println(fileAttributeView.readAttributes().isReadOnly());
+
+    }
+}
