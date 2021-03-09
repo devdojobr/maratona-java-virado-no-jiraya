74188fb4395b5f3c9250922bd69f8b81e9243ded
William Suane <william.suane@devdojo.academy>
2021-03-09 15:45:46 +0100

    video 152

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PosixFileAttributesTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+public class PosixFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("/home/william/dev/file.txt");
+        PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+        System.out.println(posixFileAttributes.permissions());
+        PosixFileAttributeView fileAttributeView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
+        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("rw-rw-rw-");
+        fileAttributeView.setPermissions(posixFilePermissions);
+        System.out.println(fileAttributeView.readAttributes().permissions());
+    }
+}
