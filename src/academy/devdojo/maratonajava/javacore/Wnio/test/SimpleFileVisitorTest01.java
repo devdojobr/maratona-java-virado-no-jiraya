3ddf9061bf8b0b574e22c6b9d45ed22414d36e5f
William Suane <william.suane@devdojo.academy>
2021-03-10 17:41:11 +0100

    video 155

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-class ListAllFiles extends SimpleFileVisitor<Path> {
+class ListJavaFiles extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         if (file.getFileName().toString().endsWith(".java")) {
@@ -18,6 +18,6 @@ class ListAllFiles extends SimpleFileVisitor<Path> {
 public class SimpleFileVisitorTest01 {
     public static void main(String[] args) throws IOException {
         Path root = Paths.get(".");
-        Files.walkFileTree(root, new ListAllFiles());
+        Files.walkFileTree(root, new ListJavaFiles());
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        System.out.println(file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("pre visit "+dir.getFileName());
+        return FileVisitResult.SKIP_SUBTREE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return super.visitFileFailed(file, exc);
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("post visit "+dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class SimpleFileVisitorTest02 {
+    public static void main(String[] args) throws IOException {
+        Path root = Paths.get("pasta");
+        Files.walkFileTree(root, new ListAllFiles());
+    }
+}
