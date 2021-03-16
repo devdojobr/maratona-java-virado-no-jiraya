e5d65d78ad180e2437c20013f297156f6c3a293a
William Suane <william.suane@devdojo.academy>
2021-03-16 17:20:55 +0100

    video 162

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -8,6 +8,19 @@ public class Smartphone {
         this.serialNumber = serialNumber;
         this.marca = marca;
     }
+    // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    // Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // Transitividade: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) tem que retornar false.
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
 
     public String getSerialNumber() {
         return serialNumber;
