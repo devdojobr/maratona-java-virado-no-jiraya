3c3b0135c766861ee99c50abab4eb08a5f144618
William Suane <william.suane@devdojo.academy>
2021-03-17 17:47:57 +0100

    video 164

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -8,6 +8,7 @@ public class Smartphone {
         this.serialNumber = serialNumber;
         this.marca = marca;
     }
+
     // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
     // Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
     // Transitividade: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
@@ -15,11 +16,20 @@ public class Smartphone {
     // para x diferente de null, x.equals(null) tem que retornar false.
     @Override
     public boolean equals(Object obj) {
-        if(obj == null) return false;
-        if(this == obj) return true;
-        if(this.getClass() != obj.getClass()) return false;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
         Smartphone smartphone = (Smartphone) obj;
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
+    // se x.equals(y) == true, y.hashCode() == x.hashCode()
+    // y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashCode() != x.hashCode() x.equals(y) deverá ser false.
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
     }
 
     public String getSerialNumber() {
