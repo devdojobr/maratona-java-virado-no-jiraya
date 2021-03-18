0d3fc3f9bc96e7a34364e778ab6d2e97e462a052
William Suane <william.suane@devdojo.academy>
2021-03-18 18:21:02 +0100

    video 167

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -5,17 +5,30 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList<>(16); // 1.5
+        List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("William");
-        nomes.add("DevDojo Academy");
+        nomes.add("DevDojo");
+        nomes2.add("Suane");
+        nomes2.add("Academy");
+
+//        System.out.println(nomes.remove("william"));
+        nomes.addAll(nomes2);
 
         for(String nome: nomes){
             System.out.println(nome);
         }
-        nomes.add("Suane");
+
         System.out.println("--------");
-        for(int i = 0; i < nomes.size(); i++){
+
+        int size = nomes.size();
+        for(int i = 0; i < size; i++){
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+
+
     }
 }
