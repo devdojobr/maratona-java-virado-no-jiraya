8892d4999e6ded2fb862dbcb3c08fbe631220296
William Suane <william.suane@devdojo.academy>
2021-03-22 17:02:04 +0100

    video 171

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
@@ -4,7 +4,16 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class MangaByIdComparator implements Comparator<Manga> {
+
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -18,6 +27,13 @@ public class MangaSortTest01 {
             System.out.println(manga);
         }
         Collections.sort(mangas);
+        System.out.println("-------------");
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+//        Collections.sort(mangas, new MangaByIdComparator());
+        mangas.sort(new MangaByIdComparator());
         System.out.println("-------------");
         for (Manga manga : mangas) {
             System.out.println(manga);
