d875f6be59376ae60cf861fdd289783349440eb8
William Suane <william.suane@devdojo.academy>
2021-03-25 17:58:14 +0100

    video 175

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -26,12 +26,12 @@ public class Manga implements Comparable<Manga> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Manga manga = (Manga) o;
-        return Double.compare(manga.preco, preco) == 0 && id.equals(manga.id) && nome.equals(manga.nome);
+        return Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome, preco);
+        return Objects.hash(id, nome);
     }
 
     @Override
--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
@@ -4,11 +4,12 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class IteratorTest01 {
     public static void main(String[] args) {
-        List<Manga> mangas = new ArrayList<>(6);
+        List<Manga> mangas = new LinkedList<>();
         mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9,0));
         mangas.add(new Manga(1L, "Berserk", 9.5,5));
         mangas.add(new Manga(4L, "Pokemon", 3.2,0));
