661e7bca67dbcd5b7a00c91eb1aaad770735505f
William Suane <william.suane@devdojo.academy>
2021-03-31 19:17:48 +0200

    video 181

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableMapTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMapTest01 {
+    public static void main(String[] args) {
+        NavigableMap<String, String> map = new TreeMap<>();
+        map.put("A", "Letra A");
+        map.put("D", "Letra D");
+        map.put("B", "Letra B");
+        map.put("C", "Letra C");
+        map.put("E", "Letra E");
+
+        for(Map.Entry<String, String> entry: map.entrySet()){
+            System.out.println(entry.getKey() +"-"+entry.getValue());
+        }
+        // lower <
+        // floor <=
+        // higher >
+        // ceiling >=
+        System.out.println(map.headMap("C", true));
+        System.out.println(map.ceilingKey("C"));
+        System.out.println(map.higherKey("C"));
+        System.out.println(map.lowerKey("C"));
+        System.out.println(map.floorKey("C"));
+    }
+}
