52feac1a74e857d7e3792ecb897d1111a8e0f0c1
William Suane <william.suane@devdojo.academy>
2021-04-05 18:45:01 +0200

    video 185

--- src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -1,19 +1,16 @@
 package academy.devdojo.maratonajava.javacore.Zgenerics.test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
-        List<Animal> animals = new ArrayList<>();
-
         printConsulta(cachorros);
         printConsulta(gatos);
-        printConsultaCachorro(cachorros);
-        printConsultaCachorro(animals);
+        List<Animal> animals = new ArrayList<>();
+        printConsultaAnimal(animals);
     }
     //Type erasure
     private static void printConsulta(List<? extends Animal> animals) {
@@ -21,17 +18,8 @@ public class WildcardTest02 {
             animal.consulta();
         }
     }
-
-    private static void printConsultaCachorro(List<? super Cachorro> animals) {
-        for (Object animal : animals) {
-            if(animal instanceof Cachorro){
-                Cachorro c = (Cachorro) animal;
-                c.consulta();
-            }
-        }
-    }
-
-    private static void customSort(List<? extends Comparable> list){
-        Collections.sort(list);
+    private static void printConsultaAnimal(List<? super Animal> animals){
+        animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
