750ba29767022b03665aae51e8901142d2397951
William Suane <william.suane@devdojo.academy>
2021-04-05 18:13:29 +0200

    video 185

--- src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -1,19 +1,37 @@
 package academy.devdojo.maratonajava.javacore.Zgenerics.test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        List<Animal> animals = new ArrayList<>();
 
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        printConsultaCachorro(cachorros);
+        printConsultaCachorro(animals);
     }
     //Type erasure
-    private static void printConsulta(List<Animal> animals) {
+    private static void printConsulta(List<? extends Animal> animals) {
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
 
-        animals.add(new Cachorro());
+    private static void printConsultaCachorro(List<? super Cachorro> animals) {
+        for (Object animal : animals) {
+            if(animal instanceof Cachorro){
+                Cachorro c = (Cachorro) animal;
+                c.consulta();
+            }
+        }
+    }
+
+    private static void customSort(List<? extends Comparable> list){
+        Collections.sort(list);
     }
 }
