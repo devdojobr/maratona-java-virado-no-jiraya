10f3c676760a7f03c8d801ad709e577e0b77a490
William Suane <william.suane@devdojo.academy>
2021-04-06 18:19:16 +0200

    video 187

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/service/RentalService.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.service;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+
+import java.util.List;
+
+public class RentalService<T> {
+    private List<T> objetosDisponiveis;
+
+    public RentalService(List<T> objetosDisponiveis) {
+        this.objetosDisponiveis = objetosDisponiveis;
+    }
+
+    public T buscarObjetoDisponivel() {
+        System.out.println("Buscando objeto disponivel....");
+        T t = objetosDisponiveis.remove(0);
+        System.out.println("Alugando objeto: " + t);
+        System.out.println("Objetos disponíveis para alugar:");
+        System.out.println(objetosDisponiveis);
+        return t;
+    }
+
+    public void retornarObjetoAlugado(T t) {
+        System.out.println("Devolvendo objeto " + t);
+        objetosDisponiveis.add(t);
+        System.out.println("Objetos disponíveis para alugar: ");
+        System.out.println(objetosDisponiveis);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClasseGenericaTest03.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.BarcoRentavelService;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.RentalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClasseGenericaTest03 {
+    public static void main(String[] args) {
+        List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW"), new Carro("Fusca")));
+        List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Canoa")));
+        RentalService<Carro> rentalServiceCarro = new RentalService<>(carrosDisponiveis);
+        Carro carro = rentalServiceCarro.buscarObjetoDisponivel();
+        System.out.println("Usando o carro por um mês...");
+        rentalServiceCarro.retornarObjetoAlugado(carro);
+
+        System.out.println("----------");
+
+        RentalService<Barco> rentalServiceBarco = new RentalService<>(barcosDisponiveis);
+        Barco barco = rentalServiceBarco.buscarObjetoDisponivel();
+        System.out.println("Usando o barco por um mês...");
+        rentalServiceBarco.retornarObjetoAlugado(barco);
+
+    }
+}
