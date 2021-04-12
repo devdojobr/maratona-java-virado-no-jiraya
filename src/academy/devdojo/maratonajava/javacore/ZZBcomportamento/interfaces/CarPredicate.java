be8f162680731faa2de8bb6ff890751335493685
William Suane <william.suane@devdojo.academy>
2021-04-12 18:50:03 +0200

    video 195

--- src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,10 @@ package academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // anonimas, funções, conciso
     boolean test(Car car);
+    //(parametro) -> <expressão>
+    //(Car car) -> car.getColor().equals("green");
 }
