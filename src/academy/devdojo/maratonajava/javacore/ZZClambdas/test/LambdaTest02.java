05b3031d92d2726ee75e08da89df57dd717d6f59
William Suane <william.suane@devdojo.academy>
2021-04-13 16:57:39 +0200

    video 197

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest02.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Natsu", "Allucard");
+        List<Integer> integers = map(strings, (String s) -> s.length());
+        List<String> map = map(strings, s -> s.toUpperCase());
+        System.out.println(integers);
+        System.out.println(map);
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T e : list) {
+            R r = function.apply(e);
+            result.add(r);
+        }
+        return result;
+    }
+}
