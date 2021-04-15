a1994bd1653ea03584f26c958d13527cf1da09e1
William Suane <william.suane@devdojo.academy>
2021-04-15 18:50:28 +0200

    video 199

--- src/academy/devdojo/maratonajava/javacore/ZZClambdas/service/AnimeComparators.java
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/service/AnimeComparators.java
@@ -10,4 +10,8 @@ public class AnimeComparators {
     public static int compareByEpisodes(Anime a1, Anime a2) {
         return Integer.compare(a1.getEpisodes(), a2.getEpisodes());
     }
+
+    public int compareByEpisodesNonStatic(Anime a1, Anime a2) {
+        return Integer.compare(a1.getEpisodes(), a2.getEpisodes());
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -6,13 +6,13 @@ import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators
 import java.util.ArrayList;
 import java.util.List;
 
-// Reference to a static method
-public class MethodReferenceTest01 {
+//Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
     public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
         List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One piece", 900), new Anime("Naruto", 500)));
-        animeList.sort((a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
-        animeList.sort(AnimeComparators::compareByTitle);
-        animeList.sort(AnimeComparators::compareByEpisodes);
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+        animeList.sort((a1,a2) -> animeComparators.compareByEpisodesNonStatic(a1,a2));
         System.out.println(animeList);
     }
 }
