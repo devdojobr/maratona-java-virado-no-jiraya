14f0d72ec07336bcffba0c96d16f6fff0ec8b73b
William Suane <william.suane@devdojo.academy>
2021-04-15 19:19:30 +0200

    video 200

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest04.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+//Reference to a constructor
+public class MethodReferenceTest04 {
+    public static void main(String[] args) {
+        Supplier<AnimeComparators> newAnimeComparators = AnimeComparators::new;
+        AnimeComparators animeComparators = newAnimeComparators.get();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One piece", 900), new Anime("Naruto", 500)));
+
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+
+        System.out.println(animeList);
+
+        BiFunction<String, Integer, Anime> animeBiFunction = (title, episodes) -> new Anime(title, episodes);
+        BiFunction<String, Integer, Anime> animeBiFunction2 = Anime::new;
+        System.out.println(animeBiFunction2.apply("Super campeões", 36));
+    }
+}
