03c64e32791621035dbc452fbc43d57f99172f7b
William Suane <william.suane@devdojo.academy>
2021-04-19 18:36:39 +0200

    video 204

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest02.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest02 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        List<String> titles = lightNovels.stream()
+                .sorted(Comparator.comparing(LightNovel::getTitle))
+                .filter(ln -> ln.getPrice() <= 4)
+                .limit(3)
+                .map(LightNovel::getTitle)
+                .collect(Collectors.toList());
+
+        System.out.println(titles);
+    }
+}
