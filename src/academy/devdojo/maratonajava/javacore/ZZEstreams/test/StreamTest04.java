70cad06a917b18cee42b4a49b12808dc5ce81bf2
William Suane <william.suane@devdojo.academy>
2021-04-20 18:38:35 +0200

    video 206

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest04.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest04 {
+    public static void main(String[] args) {
+        List<List<String>> devdojo = new ArrayList<>();
+        List<String> graphicDesigners = List.of("Wildnei Suane", "Catarina Santos", "Sandy Carolina");
+        List<String> developers = List.of("William", "David", "Harisson");
+        List<String> students = List.of("Édipo", "Gustavo Lima", "Gustavo Mendes", "Guilherme");
+        devdojo.add(graphicDesigners);
+        devdojo.add(developers);
+        devdojo.add(students);
+
+        for (List<String> people : devdojo) {
+            for (String person : people) {
+                System.out.println(person);
+            }
+        }
+        System.out.println("----");
+        devdojo.stream().flatMap(Collection::stream).forEach(System.out::println);
+
+    }
+}
