9578d27b088c808bbce4b8d8deb9f0e32badc50d
William Suane <william.suane@devdojo.academy>
2021-04-21 18:24:27 +0200

    video 208

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest06.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamTest06 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+    public static void main(String[] args) {
+        System.out.println(lightNovels.stream().anyMatch(ln -> ln.getPrice() > 9));
+        System.out.println(lightNovels.stream().allMatch(ln -> ln.getPrice() > 0));
+        System.out.println(lightNovels.stream().noneMatch(ln -> ln.getPrice() < 0));
+        lightNovels.stream()
+                .filter(ln -> ln.getPrice() > 3)
+                .findAny()
+                .ifPresent(System.out::println);
+
+        lightNovels
+                .stream()
+                .filter(ln -> ln.getPrice() > 3)
+                .sorted(Comparator.comparing(LightNovel::getPrice).reversed())
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        lightNovels
+                .stream()
+                .filter(ln -> ln.getPrice() > 3)
+                .max(Comparator.comparing(LightNovel::getPrice))
+                .ifPresent(System.out::println);
+    }
+}
