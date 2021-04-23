a6e82a5b04f784fabf84311a3bdb2d1b8f5c75ff
William Suane <william.suane@devdojo.academy>
2021-04-23 19:00:41 +0200

    video 213

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest11.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest11 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        System.out.println(lightNovels.stream().count());
+        System.out.println(lightNovels.stream().collect(Collectors.counting()));
+
+        lightNovels.stream().max(Comparator.comparing(LightNovel::getPrice)).ifPresent(System.out::println);
+        lightNovels.stream().collect(Collectors.maxBy(Comparator.comparing(LightNovel::getPrice))).ifPresent(System.out::println);
+
+        System.out.println(lightNovels.stream().mapToDouble(LightNovel::getPrice).sum());
+        System.out.println(lightNovels.stream().collect(Collectors.summingDouble(LightNovel::getPrice)));
+
+        lightNovels.stream().mapToDouble(LightNovel::getPrice).average().ifPresent(System.out::println);
+        System.out.println(lightNovels.stream().collect(Collectors.averagingDouble(LightNovel::getPrice)));
+
+        DoubleSummaryStatistics collect = lightNovels.stream().collect(Collectors.summarizingDouble(LightNovel::getPrice));
+        System.out.println(collect);
+
+        String titles = lightNovels.stream().map(LightNovel::getTitle).collect(Collectors.joining(", "));
+        System.out.println(titles);
+
+
+    }
+}
