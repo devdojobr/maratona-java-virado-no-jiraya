a6eb019ab0975249aae41f5acc863ce2a3f4a010
William Suane <william.suane@devdojo.academy>
2021-04-28 19:59:17 +0200

    video 222

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
@@ -33,6 +33,11 @@ class ThreadExampleRunnable implements Runnable {
             if(i % 100 == 0){
                 System.out.println();
             }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -43,10 +48,11 @@ public class ThreadTest01 {
 //        ThreadExample t2 = new ThreadExample('B');
 //        ThreadExample t3 = new ThreadExample('C');
 //        ThreadExample t4 = new ThreadExample('D');
-        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t2 = new Thread(new ThreadExampleRunnable('B'));
-        Thread t3 = new Thread(new ThreadExampleRunnable('C'));
-        Thread t4 = new Thread(new ThreadExampleRunnable('D'));
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'), "T1A");
+        Thread t2 = new Thread(new ThreadExampleRunnable('B'), "T2B");
+        Thread t3 = new Thread(new ThreadExampleRunnable('C'), "T3C");
+        Thread t4 = new Thread(new ThreadExampleRunnable('D'), "T4D");
+        t4.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
         t3.start();
