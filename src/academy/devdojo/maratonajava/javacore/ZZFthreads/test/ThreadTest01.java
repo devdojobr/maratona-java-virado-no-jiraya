493535fef0b453c7e81c82080830c39449000223
William Suane <william.suane@devdojo.academy>
2021-04-28 19:29:55 +0200

    video 221

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
@@ -51,5 +51,6 @@ public class ThreadTest01 {
         t2.start();
         t3.start();
         t4.start();
+        System.out.println("############################# "+Thread.currentThread().getName());
     }
 }
