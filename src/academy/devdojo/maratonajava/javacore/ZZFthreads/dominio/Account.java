4b5bebbcc53bec9c7c0b957db6f414ab047175ce
William Suane <william.suane@devdojo.academy>
2021-04-29 20:03:58 +0200

    video 224

---
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/dominio/Account.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.dominio;
+
+public class Account {
+    private int balance = 50;
+
+    public void withdrawal(int amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
+
+import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Account;
+
+public class ThreadAccountTest01 implements Runnable {
+    private Account account = new Account();
+
+    public static void main(String[] args) {
+        ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
+        Thread t1 = new Thread(threadAccountTest01, "Hestia");
+        Thread t2 = new Thread(threadAccountTest01, "Bell Cranel");
+        t1.start();
+        t2.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            withdrawal(10);
+            if (account.getBalance() < 0) {
+                System.out.println("FODEO");
+            }
+        }
+    }
+
+    private void withdrawal(int amount) {
+        if (account.getBalance() >= amount) {
+            System.out.println(getThreadName() + " está indo sacar dinheiro");
+            account.withdrawal(amount);
+            System.out.println(getThreadName() + " completou o saque, valor atual da conta " + account.getBalance());
+        } else {
+            System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+        }
+    }
+
+    private String getThreadName() {
+        return Thread.currentThread().getName();
+    }
+
+}
