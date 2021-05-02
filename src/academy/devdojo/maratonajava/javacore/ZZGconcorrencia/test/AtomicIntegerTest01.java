a5fae2971cd6ff9b0efb5febeaea42d29955c4f7
William Suane <william.suane@devdojo.academy>
2021-05-02 19:25:42 +0200

    video 230

--- src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
@@ -1,14 +1,22 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Counter {
     private int count;
     private AtomicInteger atomicInteger = new AtomicInteger();
-
+    private Lock lock = new ReentrantLock(true);
     void increment() {
-        count++;
-        atomicInteger.incrementAndGet();
+        lock.lock();
+        try{
+            count++;
+            atomicInteger.incrementAndGet();
+        }finally {
+            lock.unlock();
+        }
     }
 
     public int getCount() {
---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ReentrantLockTest01.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Worker implements Runnable {
+    private String name;
+    private ReentrantLock lock;
+
+    public Worker(String name, ReentrantLock lock) {
+        this.name = name;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+        try {
+            System.out.printf("Thread %s entrou em uma sessão crítica%n", name);
+            System.out.printf("%d Threads esperando na fila%n", lock.getQueueLength());
+            System.out.printf("Thread %s vai esperar 2s%n", name);
+            Thread.sleep(2000);
+            System.out.printf("Thread %s finalizou a espera%n", name);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+public class ReentrantLockTest01 {
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock(true);
+        new Thread(new Worker("A", lock)).start();
+        new Thread(new Worker("B", lock)).start();
+        new Thread(new Worker("C", lock)).start();
+        new Thread(new Worker("D", lock)).start();
+        new Thread(new Worker("E", lock)).start();
+        new Thread(new Worker("F", lock)).start();
+        new Thread(new Worker("G", lock)).start();
+    }
+}
