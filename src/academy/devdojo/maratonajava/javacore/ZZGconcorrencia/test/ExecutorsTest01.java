f06f2a5eedd077e625ea9d70507fe01988a76c6f
William Suane <william.suane@devdojo.academy>
2021-05-04 18:53:19 +0200

    video 236

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ExecutorsTest01.java
@@ -0,0 +1,45 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Printer implements Runnable {
+    private final int num;
+
+    public Printer(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s inicio: %d%n",Thread.currentThread().getName(), num);
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("%s finalizou%n",Thread.currentThread().getName());
+
+    }
+}
+
+public class ExecutorsTest01 {
+    public static void main(String[] args) {
+        //submission of tasks is decoupled of the execution
+
+//        System.out.println(Runtime.getRuntime().availableProcessors());
+//        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Printer(1));
+        executorService.execute(new Printer(2));
+        executorService.execute(new Printer(3));
+        executorService.execute(new Printer(4));
+        executorService.execute(new Printer(5));
+        executorService.execute(new Printer(6));
+        executorService.shutdown();
+
+        System.out.println("Programa finalizado");
+    }
+}
