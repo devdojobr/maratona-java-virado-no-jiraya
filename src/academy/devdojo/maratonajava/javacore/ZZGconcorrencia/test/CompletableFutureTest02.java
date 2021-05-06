e0f275ab2219a68cf99493c180e104b306cb880a
William Suane <william.suane@devdojo.academy>
2021-05-06 18:17:16 +0200

    video 241

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CompletableFutureTest02.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service.StoreService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CompletableFutureTest02 {
+    public static void main(String[] args) {
+        StoreService storeService = new StoreService();
+        searchPricesAsyncCompletableFuture(storeService);
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreService storeService) {
+        long start = System.currentTimeMillis();
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(storeService::getPricesAsyncCompletableFuture)
+                .collect(Collectors.toList());
+
+        List<Double> prices = completableFutures.stream().map(CompletableFuture::join).collect(Collectors.toList());
+
+
+        System.out.println(prices);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
+    }
+}
