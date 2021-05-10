75f97a7d8dd4028287258dbbc56ee05f7a2b732e
William Suane <william.suane@devdojo.academy>
2021-05-10 19:17:17 +0200

    video 246

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/test/BuilderPatternTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio.Person;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        Person build = new Person.PersonBuilder()
+                .firstName("William")
+                .lastName("Suane")
+                .username("ViradoNoJiraya")
+                .email("william.suane@devdojo.academy")
+                .build();
+        System.out.println(build);
+
+    }
+}
