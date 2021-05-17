285b7207af822c2db8354217010452f2a37ce4db
William Suane <william.suane@devdojo.academy>
2021-05-17 19:25:07 +0200

    video 255

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.conn;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+    //java.sql = Connection, Statement, ResultSet, DriverManager
+
+}
