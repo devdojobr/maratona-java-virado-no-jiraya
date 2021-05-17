0a6eaf6d6eba11321643d831e4c06a9cd6d409af
William Suane <william.suane@devdojo.academy>
2021-05-17 21:58:46 +0200

    video 258

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -2,18 +2,20 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer) {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,12 +1,13 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }
